6559d8f69bb3f83f726b1ef8ab4dd442095ceae0
ssuhacho <ssuhacho@gmail.com>
2023-04-13 16:52:05 -0500

    [backend] update/delete job type added

--- backend/controllers/jobsTypeController.js
+++ backend/controllers/jobsTypeController.js
@@ -31,6 +31,31 @@ exports.allJobsType = async (req, res, next) => {
     }
 }
 
+//update jobs category
+exports.updateJobsType = async (req, res, next) => {
+    try {
+        const jobT = await JobType.findByIdAndUpdate(req.params.type_id, req.body, {new: true});
+        res.status(200).json({
+            success: true,
+            jobT
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+//delete jobs category
+exports.deleteJobsType = async (req, res, next) => {
+    try {
+        const jobT = await JobType.findByIdAndRemove(req.params.type_id);
+        res.status(200).json({
+            success: true,
+            message: "Job type has been deleted"
+        })
+    } catch (error) {
+        next(new ErrorResponse("server error", 500));
+    }
+}
 
 
 
--- backend/routes/jobsTypeRoutes.js
+++ backend/routes/jobsTypeRoutes.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { createJobType, allJobsType } = require('../controllers/jobsTypeController');
-const { isAuthenticated } = require('../middleware/auth');
+const { createJobType, allJobsType, updateJobsType, deleteJobsType } = require('../controllers/jobsTypeController');
+const { isAuthenticated, isAdmin } = require('../middleware/auth');
 
 
 
 //job type routes
 
 // /api/type/create
-router.post('/type/create', isAuthenticated, createJobType)
+router.post('/type/create', isAuthenticated, isAdmin, createJobType)
 // /api/type/jobs
 router.get('/type/jobs', allJobsType)
-
+// /api/type/update/:type_id
+router.put('/type/update/:type_id', isAuthenticated, isAdmin, updateJobsType)
+// /api/type/update/:type_id
+router.delete('/type/delete/:type_id', isAuthenticated, isAdmin, deleteJobsType)
 
 
 
